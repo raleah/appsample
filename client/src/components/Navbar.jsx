353afba4cc43f54845d10e7c3426265e149a770c
Raleah <raleah9@gmail.com>
2022-02-07 23:54:00 -0500

    0.1

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+
+import React from 'react';
+import { HiMenu } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
+
+import logo from '../../images/panthersize.png'; 
+
+const NavbarItem = ({title, classProps}) => {
+  return(
+    <li className={`mx-4 cursor-pointer ${classProps}`}>
+      {title}
+    </li>)
+}
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = React.useState(false);
+
+    return (
+      <nav className="w-full flex md:justify-center justify-between items-center p-4">
+        <div className="md:flex-[0.5] flex-initial justify-center items-center">
+        <img src={logo} alit="logo" className="w-32 cursor-pointer"></img>
+          </div> 
+          <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
+          {["Market", "Exchange","NFTs", "Wallets"].map((item, index)=> (
+            <NavbarItem key={item +index} title={item}/>
+            ))}
+
+            <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
+              Login
+            </li>
+          
+          </ul>
+
+           <div className="flex relative">
+          {toggleMenu
+          ? <AiOutlineClose fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(false)}/>
+          : <HiMenu fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(true)} />
+          }     
+          </div>
+        </nav>
+        );
+}
+
+export default Navbar;
